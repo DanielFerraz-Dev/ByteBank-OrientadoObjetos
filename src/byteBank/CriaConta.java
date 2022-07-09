75c495b923a894f5ed9805dd074f896938196868
DanielFerraz-Dev <danielmasterhd1999@gmail.com>
2022-07-08 23:03:40 -0300

    Update CriaConta.java
    Aplicação do método .transfere

--- src/byteBank/CriaConta.java
+++ src/byteBank/CriaConta.java
@@ -10,6 +10,11 @@ public class CriaConta {
     boolean exitoOperacao = contaDaniel.saca(121);
     System.out.println(contaDaniel.saldo);
     System.out.println(exitoOperacao);
+    
+    Conta contaJanael = new Conta();
+    contaJanael.deposita(500);
+    contaJanael.transfere(200, contaDaniel);
+    System.out.println(contaJanael.saldo + " " + contaDaniel.saldo);
 	}
 
 }
